4716141785a8071f482b7fa7328a73704e65cb4f
Kapil Shah <kapilshah111991@gmail.com>
2026-02-15 13:53:33 +0530

    Refactor Code

---
+++ src/components/BGColor.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function BGColor() {
+  const [color, setColor] = useState("red");
+
+  return (
+    <>
+      <div style={{ backgroundColor: color, height: 200, width: "100%" }}></div>
+      <br />
+      <button
+        onClick={() => setColor("red")}
+        className={color === "red" ? "selectedButton" : ""}
+      >
+        Red
+      </button>
+      <button
+        onClick={() => setColor("green")}
+        className={color === "green" ? "selectedButton" : ""}
+      >
+        Green
+      </button>
+      <button
+        onClick={() => setColor("blue")}
+        className={color === "blue" ? "selectedButton" : ""}
+      >
+        Blue
+      </button>
+    </>
+  );
+}
+
+export default BGColor;
